f8ba7825d7bffe107f85fa2b7f0d2fc7207b66aa
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2012-01-12 22:34:38 -0500

    Switch dev/prod paths (oups!)

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,4 +1,4 @@
 server "revengeanceduchesses@int-web-1.monhs.com", :web, :app, :db, :primary=>true
-set :hostname, "prod.revengeanceduchesses.monhs.com"
-set :deploy_to, "/home/revengeanceduchesses/domains/prod.revengeanceduchesses.monhs.com/"
+set :hostname, "dev.revengeanceduchesses.monhs.com"
+set :deploy_to, "/home/revengeanceduchesses/domains/dev.revengeanceduchesses.monhs.com/"
 set :keep_releases, 1
--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,5 +1,5 @@
 server "revengeanceduchesses@int-web-1.monhs.com", :web, :app, :db, :primary=>true
-set :hostname, "dev.revengeanceduchesses.monhs.com"
-set :deploy_to, "/home/revengeanceduchesses/domains/dev.revengeanceduchesses.monhs.com/"
+set :hostname, "prod.revengeanceduchesses.monhs.com"
+set :deploy_to, "/home/revengeanceduchesses/domains/prod.revengeanceduchesses.monhs.com/"
 set :keep_releases, 5
 set :branch, "production"
