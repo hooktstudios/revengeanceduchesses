640bae6b2fe3fe067b8e0d113b17b4770c6c6b2e
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2012-01-16 18:23:28 -0500

    Allow contributors to upload their medias

--- wp-content/themes/revengeance/functions.php
+++ wp-content/themes/revengeance/functions.php
@@ -592,3 +592,10 @@ function twentyeleven_body_classes( $classes ) {
 }
 add_filter( 'body_class', 'twentyeleven_body_classes' );
 
+if ( current_user_can('contributor') && !current_user_can('upload_files') )
+        add_action('admin_init', 'allow_contributor_uploads');
+
+function allow_contributor_uploads() {
+        $contributor = get_role('contributor');
+        $contributor->add_cap('upload_files');
+}
