c6a2ab446abd6f1aa83dcb5881bf8c9fb55f24a3
Léo Renaud Allaire <leo.renaud@hooktstudios.com>
2012-01-22 12:32:18 -0500

    Only show last post on home

--- wp-content/themes/revengeance/index.php
+++ wp-content/themes/revengeance/index.php
@@ -15,14 +15,8 @@
 get_header(); ?>
 			<div class="content">
 				<?php if ( have_posts() ) : ?>
-
-					<?php /* Start the Loop */ ?>
-					<?php while ( have_posts() ) : the_post(); ?>
-
-						<?php get_template_part( 'content', get_post_format() ); ?>
-
-					<?php endwhile; ?>
-
+					<?php the_post(); ?>
+					<?php get_template_part( 'content', get_post_format() ); ?>
 				<?php endif; ?>
 			</div>
 
