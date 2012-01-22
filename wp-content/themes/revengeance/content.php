fe7c0eb16cae03dfa5f4feced3189c3c7b262108
Léo Renaud Allaire <leo.renaud@hooktstudios.com>
2012-01-22 12:32:57 -0500

    Remove bunch of WP crap + style comment link

--- wp-content/themes/revengeance/content.php
+++ wp-content/themes/revengeance/content.php
@@ -16,46 +16,16 @@
 				<time class="entry-date" datetime="<?php echo esc_attr(get_the_date('c')) ?>" pubdate><?php echo esc_attr(get_the_date()) ?></time>
 			<?php endif; ?>
 		</header>
-
-		<?php if ( is_search() ) : // Only display Excerpts for Search ?>
-		<div class="entry-summary">
-			<?php the_excerpt(); ?>
-		</div><!-- .entry-summary -->
-		<?php else : ?>
 		<div class="entry-content">
 			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
-		</div><!-- .entry-content -->
-		<?php endif; ?>
-
+		</div>
 		<footer class="entry-meta">
-			<?php $show_sep = false; ?>
-			<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'twentyeleven' ) );
-				if ( $tags_list ):
-				if ( $show_sep ) : ?>
-			<span class="sep"> | </span>
-				<?php endif; // End if $show_sep ?>
-			<span class="tag-links">
-				<?php printf( __( '<span class="%1$s">Tagged</span> %2$s', 'twentyeleven' ), 'entry-utility-prep entry-utility-prep-tag-links', $tags_list );
-				$show_sep = true; ?>
-			</span>
-			<?php endif; // End if $tags_list ?>
-			<?php endif; // End if 'post' == get_post_type() ?>
-
 			<?php if ( comments_open() ) : ?>
-			<?php if ( $show_sep ) : ?>
-			<span class="sep"> | </span>
-			<?php endif; // End if $show_sep ?>
-			<span class="comments-link"><?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'twentyeleven' ) . '</span>', __( '<b>1</b> Reply', 'twentyeleven' ), __( '<b>%</b> Replies', 'twentyeleven' ) ); ?></span>
-			<?php endif; // End if comments_open() ?>
-
-			<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
-		</footer><!-- #entry-meta -->
-	</article><!-- #post-<?php the_ID(); ?> -->
+			<?php comments_popup_link( '0', '1','%', 'comment-link'); ?>
+			<?php endif;?>
+		</footer>
+	</article>
 </div>
 <nav id="duchesses-nav">
 	<h2>Vos <br>duchesses</h2>
