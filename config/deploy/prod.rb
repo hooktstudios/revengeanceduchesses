ab668cc2e672a3c03cb0e21c959aecdb5e5d0590
Jean-Philippe Doyle <jeanphilippe.doyle@hooktstudios.com>
2012-08-30 15:54:19 -0400

    Keep 2 releases

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,5 +1,5 @@
 server "revengeanceduchesses@int-web-1.monhs.com", :web, :app, :db, :primary=>true
 set :hostname, "prod.revengeanceduchesses.monhs.com"
 set :deploy_to, "/home/revengeanceduchesses/domains/prod.revengeanceduchesses.monhs.com/"
-set :keep_releases, 5
+set :keep_releases, 2
 set :branch, "master"
